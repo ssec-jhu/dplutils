a3b45290f7402ce8d3192f250ea4fa864e70b4f4
Arik Mitschang <arik@jhu.edu>
2024-01-23 16:03:47 -0500

    fixing missing line coverage (can't catch this in ray)

--- tests/pipeline/test_stream_executor.py
+++ tests/pipeline/test_stream_executor.py
@@ -16,3 +16,11 @@ def test_stream_task_wrapper():
     st.pending.append(1)
     st.split_pending.append(1)
     assert st.total_pending() == 3
+
+
+def test_stream_exhausted_indicator_considers_splits(dummy_steps):
+    pl = LocalSerialExecutor(dummy_steps)
+    a_task = pl.stream_graph.task_map['task2']
+    assert pl.task_exhausted(a_task)
+    a_task.split_pending.append(1)
+    assert not pl.task_exhausted(a_task)
